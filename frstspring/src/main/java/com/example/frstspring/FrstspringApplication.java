ed47e2b0b45af92837c30d4f5e5e9e42fb60e541
Nmstacy22 <158740355+Nmstacy22@users.noreply.github.com>
2025-07-14 18:33:44 +0000

    Created first springboot project

---
+++ frstspring/src/main/java/com/example/frstspring/FrstspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.frstspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrstspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrstspringApplication.class, args);
+	}
+
+}
---
+++ frstspring/src/test/java/com/example/frstspring/FrstspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frstspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrstspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
