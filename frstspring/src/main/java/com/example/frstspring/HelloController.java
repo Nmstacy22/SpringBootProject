6115f15b8a909841dc0dc710e3d2e267134659eb
Nmstacy22 <158740355+Nmstacy22@users.noreply.github.com>
2025-07-15 10:36:12 +0000

    Adding a post request using @PostMapping and also changing to get a JSON responce on our browser

--- frstspring/src/main/java/com/example/frstspring/HelloController.java
+++ frstspring/src/main/java/com/example/frstspring/HelloController.java
@@ -2,12 +2,21 @@ package com.example.frstspring;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
+    public HelloResponse hello() {
+        return new HelloResponse("Hello, World!");
     }
+
+    @PostMapping("/hello")
+    public HelloResponse HelloPost(@RequestBody String name){
+        return new HelloResponse("Hello"+ name + "!");
+    }
+    
 }
