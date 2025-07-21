b9c29e041b867e9b0eb967c3635f6f8f92ccaaf3
MN Stacy <ndangmbistacy@gmail.com>
2025-07-21 23:34:12 +0100

    updated a category using @PutMapping and updateCategory method

--- sbecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sbecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
@@ -46,6 +48,16 @@ public class CategoryController{
             return ResponseEntity.status(HttpStatus.OK).body(status);
         } catch (ResponseStatusException e) {
             //return new ResponseEntity<>(e.getReason(),e.getStatusCode());
+            return ResponseEntity.status(e.getStatusCode()).body(e.getReason());
+        }
+    }
+
+    @PutMapping("/api/public/categories/{categoryId}")
+    public ResponseEntity<String> updateCategory(@RequestBody Category category, @PathVariable Long categoryId){
+        try{
+            Category savedCategory = categoryService.updateCategory(category, categoryId);
+            return ResponseEntity.status(HttpStatus.OK).body("Category with categoryId " + categoryId + " updated successfully");
+        } catch (ResponseStatusException e) {
             return ResponseEntity.status(e.getStatusCode()).body(e.getReason());
         }
     }
--- sbecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ sbecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.ecommerce.project.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -36,5 +37,23 @@ public class CategoryServiceImpl implements CategoryService {
             categories.remove(category);
             return "Category with categoryId: "+ categoryId + " deleted successfully";
     }
+
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        Optional<Category> optionalCategory = categories.stream()
+        .filter(c -> c.getCategoryId().equals(categoryId))
+        .findFirst();
+        if (optionalCategory.isPresent()) {
+            Category existingCategory = optionalCategory.get();
+            existingCategory.setCategoryName(category.getCategoryName());
+            return existingCategory;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found");
+        }
+
+    } 
+
+    
 
 }
